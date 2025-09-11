4e0a95dba9d36f5d6106ab940a2a41649a3357c6
Md Faiaz Fahim <faiazfahim64@gmail.com>
2025-09-11 12:47:30 +0600

    Changes

--- Run_and_Dodge.cpp
+++ Run_and_Dodge.cpp
@@ -3,16 +3,26 @@
 int main()
 {
     //Screen Initalization
-    int width = 800;
-    int height = 450;
-    int width_half = width/2;
-    int height_half = height/2;
+    const int width = 800;
+    const int height = 450;
+    const int width_half = width/2;
+    const int height_half = height/2;
 
-    int r_width = 50;
-    int r_height = 80;
+    //Character height and width
+    const int r_width = 50;
+    const int r_height = 80;
 
+    //Character Jumping
     int posY = height - r_height;
     int velocity = 0;
+    const int jumpVelocity = -22;
+
+    //acceletation due to gravity (pixels/frame)/frame
+    int gravity = 1;
+    
+    //Is Character in the air?
+    bool isInAir;
+
 
     //Creating Window                            
     InitWindow(width, height, "Endless Runner 2D");
@@ -26,10 +36,27 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
-        if (IsKeyPressed(KEY_SPACE))
+        //Perform Ground Check
+        if (posY >= height - r_height)
         {
-            velocity -= 10;
+            //Character on ground
+            velocity = 0;
+            isInAir = false;
         }
+        else
+        {
+            //Character in air
+            //Apply gravity
+            velocity += gravity;
+            isInAir = true;
+        }
+
+        if ( (IsKeyPressed(KEY_SPACE)) && (isInAir == false) )
+        {
+            velocity += jumpVelocity;
+        }
+
+        //Updating Position
         posY += velocity;
 
         DrawRectangle(width_half, posY, r_width, r_height, BLUE);
